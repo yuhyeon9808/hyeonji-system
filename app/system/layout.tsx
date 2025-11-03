209e787272914535efdab4d611ddd26cd7a50465
yuhyeon9808 <bwj9808@gmail.com>
2025-11-03 22:22:38 +0900

    feat: about 페이지 디자인 전면 개편

--- app/system/layout.tsx
+++ app/system/layout.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="w-full  flex justify-center bg-[#E6E6E6]">
+    <div className="w-full h-auto flex justify-center bg-[#E6E6E6]">
       <div className="md:px-5 lg:my-10 max-w-[782px] w-full h-auto bg-white-soft lg:border border-gray-line ">
         {children}
       </div>
