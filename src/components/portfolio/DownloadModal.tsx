5422adc0037f01d6a95fd167798e6e95c7225b3f
yuhyeon9808 <bwj9808@gmail.com>
2025-11-23 17:00:22 +0900

    chore 포폴 문서 용량 줄이기

--- src/components/portfolio/DownloadModal.tsx
+++ src/components/portfolio/DownloadModal.tsx
@@ -6,7 +6,7 @@ import { usePathname } from 'next/navigation';
 export default function DownloadModal({ onClose }: { onClose: () => void }) {
   const handleDownload = () => {
     const link = document.createElement('a');
-    link.href = '/portfolio.pdf';
+    link.href = '/유현지_포트폴리오.pdf';
     link.download = '유현지_포트폴리오.pdf';
     link.click();
   };
